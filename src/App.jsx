24d2e083786b3559e62299270c9d7ffd7a2903d1
Dzarooo <kacperjaroszewski06@gmail.com>
2025-07-24 11:49:31 +0200

    made home site

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import "./App.css";
 import { Header } from "./components/Header.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import { Home } from "./components/Home.jsx";
+import { Home } from "./components/Home.jsx";
 import { Manual } from "./components/Manual.jsx";
 // import { Dog } from "./components/Dog.jsx";
 import { AboutUs } from "./components/AboutUs.jsx";
-import { useState } from 'react'
-import './App.css'
 
 function App() {
   return (
@@ -15,7 +13,7 @@ function App() {
       <Header />
 
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/manual" element={<Manual />} />
         {/* <Route path="/dog" element={<Dog />} /> */}
         <Route path="/aboutus" element={<AboutUs />} /> 
